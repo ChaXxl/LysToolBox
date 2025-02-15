f502a2d41cd9ddcee32578d641fa2954c8c540df
ChaXxl <x734461204@gmail.com>
2025-02-15 16:43:17 +0800

    feat: 增加既可以选择 Excel 文件也可以选择文件夹的可拖拽 LineEdit

--- view/components/dropable_lineEdit.py
+++ view/components/dropable_lineEdit.py
@@ -126,3 +126,31 @@ class DropableLineEditExcel(LineEdit):
                 self.setText(filename)
         else:
             event.ignore()
+
+class DropableLineEditExcelDir(DropableLineEdit):
+    """
+    可拖拽的 Excel 文件和文件夹选择器. 既可以选择文件夹, 也可以选择 Excel 文件
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    @override
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls():
+            urls = event.mimeData().urls()
+
+            if not urls:
+                return
+
+            url = urls[0]
+
+            if Path(url.toLocalFile()).is_dir():
+                self.setText(url.toLocalFile())
+            else:
+                if not url.toLocalFile().endswith(".xlsx"):
+                    return
+
+                self.setText(url.toLocalFile())
+        else:
+            event.ignore()
