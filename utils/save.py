6069326910dc76ce96ddc0805d9a35e0c54d8de6
ChaXxl <734461204@qq.com>
2025-04-01 09:57:35 +0800

    refactor: 在保存数据到 Excel 时, 添加 Excel 文件格式化功能，设置列宽和对齐方式

--- utils/save.py
+++ utils/save.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 from typing import List, Any, Optional
 
-import openpyxl
+from openpyxl import load_workbook
+from openpyxl.styles import Alignment, Font
 import polars as pl
 import shortuuid
 from loguru import logger
@@ -88,3 +89,46 @@ class Save:
 
         msg = f"\n\n{self.filename.stem} {tag}-保存了 {saved_count} 条, 数据总条数: {combined_df.shape[0]}\n\n"
         self.logInfo.emit(msg)
+
+        # 格式化 Excel 文件
+        try:
+            wb = load_workbook(self.filename)
+            ws = wb.active
+
+            # 设置缩放为 100%
+            ws.sheet_view.zoomScale = 100
+
+            # 设置第一行字体及对齐方式
+            for cell in ws[1]:
+                cell.alignment = Alignment(horizontal="center", vertical="center")
+                cell.font = Font(size=15, bold=True)
+
+            # 设置第一行的行高
+            ws.row_dimensions[1].height = 25
+
+            # 定义列宽和对齐方式
+            column_formats = {
+                "A": 30,  # uuid
+                "B": 45,  # 药店名称
+                "C": 20,  # 店铺主页
+                "D": 50,  # 资质名称
+                "E": 35,  # 药品名称
+                "F": 15,  # 药品ID
+                "G": 20,  # 挂网价格
+                "H": 15,  # 平台
+                "I": 15,  # 排查日期
+            }
+
+            for col, width in column_formats.items():
+                ws.column_dimensions[col].width = width
+                for row in ws.iter_rows(min_row=2):
+                    for cell in row:
+                        cell.alignment = Alignment(
+                            horizontal="center", vertical="center"
+                        )
+
+            # 保存格式化后的文件
+            wb.save(self.filename)
+        except Exception as e:
+            self.logInfo.emit(f"格式化Excel文件失败: {e}")
+            logger.error(f"格式化Excel文件失败: {e}")
